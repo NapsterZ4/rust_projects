fb5eb00cd2179bbf09c050242cf9bac1d8965164
Napster777 <jorge.zapata@protonmail.com>
2020-11-16 18:18:52 -0600

    Split convert_string function

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,37 @@
 fn main() {
-    let bytes_message = convert_binary("Napster");
-    let key = convert_binary("Javierr");
+    let bytes_message = convert_bytes("Napster");
+    let key = convert_bytes("Javierr");
     
     let encrypt_message = xor(bytes_message, key);
+    let string_message = convert_string(encrypt_message);
 
-    let encrypt_message_xor = convert_binary(encrypt_message.as_str());
+    println!("Convert String: {:?}", string_message);
+
+    let encrypt_message_xor = convert_bytes(&*string_message);
 
     let decrypt_message = xor(encrypt_message_xor, key);
+    let decrypt_string_message = convert_string(decrypt_message);
 
-    println!("Encrypt is: {:?}", encrypt_message);
-    println!("Decrypt is: {:?}", decrypt_message);
+    println!("Convert String: {:?}", decrypt_string_message);
 }
 
-
-fn convert_binary(message: &str) -> &[u8]{
+fn convert_bytes(message: &str) -> &[u8]{
    let message_bytes = message.as_bytes();
    return message_bytes;
 }
 
 
-fn xor(message: &[u8], key: &[u8]) -> String{
+fn xor(message: &[u8], key: &[u8]) -> Vec<u8> {
     let mut xor: Vec<u8> = Vec::new();
 
     for n in 0.. message.len(){
         xor.push(message[n] ^ key[n]);
     }
 
-    return String::from_utf8(xor).unwrap();
+    xor.to_vec()
+}
+
+
+fn convert_string(message: Vec<u8>) -> String{
+    String::from_utf8(message).unwrap()
 }
