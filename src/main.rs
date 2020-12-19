1cebc445dea9e898c0263b9650501af6def53a22
Napster777 <jorge.zapata@protonmail.com>
2020-12-19 16:44:37 -0600

    Fix Vec and &[u8] error

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,43 @@ const MODULUS: i32 = 36389;
 const SEED: &[u8] = "1001100001110001".as_bytes();
 
 
-fn h_fn<'a>(first_half: &'a[u8], second_half: &'a[u8]) -> &'a[u8] {
-    let mut mod_exp: i32 = 2_i32.pow(first_half.len().try_into().unwrap()) % MODULUS;
+fn h_fn<'a>(first_half: &'a[u8], second_half: &'a[u8]) -> Vec<u8> {
+    let mod_exp: i32 = 2_i32.pow(first_half.len().try_into().unwrap()) % MODULUS;
     let mut hard_code_bit: u8 = 0;
 
     for i in 0..first_half.len() {
         hard_code_bit = hard_code_bit ^ ((first_half[i]) & (second_half[i])) % 2;
     }
 
-    mod_exp + second_half + hard_code_bit
+    let exp = mod_exp.to_be_bytes();
+    let hard = hard_code_bit.to_be_bytes();
+    let c_vec = concatenate_arrays(&exp, &second_half);
+    let final_vec = concatenate_arrays(&c_vec, &hard);
+
+    final_vec
 }
 
-fn g_fn(initial_seed: &[u8]) -> &[u8] {
-    let mut binary_string: &[u8] = initial_seed;
-    let mut result: &[u8] = b"";
+fn convert_string(message: Vec<u8>) -> String{
+    String::from_utf8(message).unwrap()
+}
 
-    for i in 0..10 {
-        let mut first_half: &[u8] = &binary_string[..(binary_string.len()) / 2];
-        let mut second_half: &[u8] = &binary_string[(binary_string.len()) / 2..];
+fn concatenate_arrays<'a, T: Clone>(x: &'a[T], y: &'a[T]) -> Vec<T> {
+    let mut concat = x.to_vec();
+    concat.extend_from_slice(y);
+
+    concat
+}
+
+fn g_fn(initial_seed: &[u8]) -> Vec<u8> {
+    let mut binary_string: Vec<u8> = initial_seed.to_vec();
+
+    for _i in 0..10 {
+        let first_half: &[u8] = &binary_string[..(binary_string.len()) / 2];
+        let second_half: &[u8] = &binary_string[(binary_string.len()) / 2..];
         binary_string = h_fn(first_half, second_half);
-        result += binary_string[-1];
-        binary_string = binary_string[..-1];
     }
 
-    result
+    binary_string
 }
 
 fn xor(message: &[u8], key: &[u8]) -> Vec<u8> {
